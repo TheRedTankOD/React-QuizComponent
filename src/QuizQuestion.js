492d77f75ab8e4685a4d3bcfae1b212efaae8b1f
TheRedTankOD <contact@oliverdaniels.co.uk>
2019-05-01 13:40:31 +0100

    Completed the module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -10,7 +10,7 @@ class QuizQuestion extends Component {
         </section>
         <section>
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
           </ul>
         </section>
       </main>
